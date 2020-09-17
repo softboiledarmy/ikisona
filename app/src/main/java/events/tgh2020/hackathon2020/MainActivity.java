4463c6b8f97bdef63792af463dc8057360b7cae6
kyanachana <tomomin001104@gmail.com>
2020-09-17 19:39:06 +0900

    todo

--- app/src/main/java/events/tgh2020/hackathon2020/MainActivity.java
+++ app/src/main/java/events/tgh2020/hackathon2020/MainActivity.java
@@ -6,8 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +27,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -31,16 +37,101 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+
+
+///////////////////////////////////////////////////////////
+        // ListViewに表示する項目を生成
+        final ArrayList<String> noodleList= new ArrayList<>();
+        noodleList.add("ごはんたべる");
+        noodleList.add("あさおきる");
+        noodleList.add("本を40P読む");
+        noodleList.add("先輩にメールを出す");
+        noodleList.add("出前を注文する");
+        noodleList.add("ホームパーティーを開く");
+
+        /**
+         * Adapterを生成
+         * android.R.layout.simple_list_item_1 : リストビュー自身のレイアウト。今回はAndroid標準のレイアウトを使用。
+         * noodleList : Adapterのコンストラクタの引数としてListViewに表示する項目のリストを渡す。
+         */
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, noodleList);
+
+        // idがlistのListViewを取得
+        final ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(arrayAdapter);
+
+        // リスト項目を長押しクリックした時の処理
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            /**
+             * @param parent ListView
+             * @param view 選択した項目
+             * @param position 選択した項目の添え字
+             * @param id 選択した項目のID
+             */
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String deleteItem = (String)((TextView)view).getText();
+
+                // 項目を追加する
+//                arrayAdapter.add("「"+deleteItem + "」を達成したよ！");
+
+                // 選択した項目を削除する
+                arrayAdapter.remove(deleteItem);
+
+                return false;
+            }
+        });
+
+        ///////////////////////////////////////////
+        final EditText praise = findViewById(R.id.praise);
+        final TextView textView = findViewById(R.id.praisetextView);
+        final Button button = findViewById(R.id.button);
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                textView.setVisibility(View.VISIBLE);
+                button.setVisibility(View.VISIBLE);
+                praise.setVisibility(View.VISIBLE);
+
+
             }
         });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String task = praise.getText().toString();
+                noodleList.add(task);
+                listView.setAdapter(arrayAdapter);
+                praise.setText("");
+                button.setVisibility(View.INVISIBLE);
+                textView.setVisibility(View.INVISIBLE);
+                praise.setVisibility(View.INVISIBLE);
+
+
+
+
+            }
+        });
+
+
+        ///////////////////////////////////////////
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
